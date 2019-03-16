413ea3dd8366016708d93d12c58caa8f594db956
AlynxZhou <alynx.zhou@gmail.com>
2019-03-16 21:48:54 +0800

    Added reward and updated UI and updated version.

--- app/src/main/java/xyz/alynx/livewallpaper/RewardActivity.java
+++ app/src/main/java/xyz/alynx/livewallpaper/RewardActivity.java
@@ -1,13 +1,37 @@
+/**
+ * Copyright 2019 Alynx Zhou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package xyz.alynx.livewallpaper;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RewardActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reward);
+        Toolbar toolbar = findViewById(R.id.reward_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(R.string.action_reward);
+        }
     }
 }
